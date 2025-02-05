d8a64e0b9e78f7d1c2266eb27c60d24bd21c65ec
Monique Grinstein <monique.grinstein@lego.com>
2025-02-05 14:49:00 +0000

    fix: reverse the lat array

--- plotter.py
+++ plotter.py
@@ -15,6 +15,7 @@ def plot_data(data, coordinates, category_index=0, band_index=0):
     
     lon = np.linspace(coordinates["west"], coordinates["east"], data_2d.shape[1])
     lat = np.linspace(coordinates["south"], coordinates["north"], data_2d.shape[0])
+    lat = lat[::-1] # Reverse the latitude array
     lon, lat = np.meshgrid(lon, lat)
     
     c = ax.pcolormesh(lon, lat, data_2d, transform=ccrs.PlateCarree(), cmap='viridis')
